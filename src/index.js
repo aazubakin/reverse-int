6a2635f9b21ac153d2376d0ed9fefa091c1770ad
Aleksandr Zubakin <eldoson@yandex.ru>
2020-10-10 22:06:58 +0500

    feat: reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let str = String(Math.abs(n));
+    let result = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        result += str[i];
+    }
+    return result;
 }
+
+console.log(module.exports(154));
